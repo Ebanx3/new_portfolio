c06be5b773242b931c161c28fbe1bdcb35744486
Ebanx3 <esteban2santos@gmail.com>
2023-05-07 20:48:06 -0300

    added nosApunter to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,57 @@
 const Portfolio = ({ inEsp, darkMode }) => {
   return (
     <div>
-      <div className="flex justify-center flex-col lg:flex-row items-center mb-5 w-full 2xl:w-4/5 m-auto p-2">
+      <div className="flex justify-center flex-col lg:flex-row items-center mb-5 w-full 2xl:w-11/12 m-auto p-2 mt-8">
+        <img src="nosApunter.png" alt="" className="w:full lg:w-1/2 " />
+        <div className="w-full lg:w-1/2 p-8 text-lg">
+          <p className={darkMode ? "text-white" : ""}>
+            {inEsp
+              ? "La idea del proyecto era suplantar un poco los apuntes que hacía durante los cursos, y que voy revisando constantemente para recordar como hacer algunas cosas. Permite crear posts con mini tutoriales sobre como usar alguna herramienta especifica. Consta con un sistema de registro y logueo, un buscador, los posts pueden recibir checks o cruces, además se pueden guardar posts en favoritos."
+              : "The idea for this site it´s have a place where to save and share mini tutorials about a specific technology. Here we can signup, login, create a post, react to a post with check or a cross and save as favourite"}
+          </p>
+          <ul className={darkMode ? "text-white " : ""}>
+            {inEsp ? "Tecnologías empleadas" : "Technologies used"}
+            <li className="ml-2">- React</li>
+            <li className="ml-2">- NextJs</li>
+            <li className="ml-2">- Cookie</li>
+            <li className="ml-2">- Eslint</li>
+            <li className="ml-2">- Mongoose</li>
+            <li className="ml-2">- Bcrypt</li>
+            <li className="ml-2">- Jsonwebtoken</li>
+            <li className="ml-2">- Typsecript</li>
+            <li className="ml-2">- tailwindcss</li>
+          </ul>
+          <div className="flex justify-end mt-8">
+            <a
+              href="https://nosapuntes.netlify.app"
+              className={
+                darkMode
+                  ? "font-bold mr-4 text-cyan-400 hover:text-cyan-200"
+                  : "font-bold mr-4 text-sky-600 hover:text-sky-400"
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              DEPLOY
+            </a>
+            <a
+              href="https://github.com/Ebanx3/devblog"
+              className={
+                darkMode
+                  ? "font-bold text-cyan-400 hover:text-cyan-200"
+                  : "font-bold text-sky-600 hover:text-sky-400"
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              REPO
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center flex-col lg:flex-row items-center mb-5 w-full 2xl:w-11/12 m-auto p-2">
         <img src="code-foro.png" alt="" className="w:full lg:w-1/2 " />
         <div className="w-full lg:w-1/2 p-8 text-lg">
           <p className={darkMode ? "text-white" : ""}>
@@ -51,7 +101,7 @@ const Portfolio = ({ inEsp, darkMode }) => {
         </div>
       </div>
 
-      <div className="flex justify-center flex-col lg:flex-row items-center mb-5 w-full 2xl:w-4/5 m-auto p-2 mt-8">
+      <div className="flex justify-center flex-col lg:flex-row items-center mb-5 w-full 2xl:w-11/12 m-auto p-2 mt-8">
         <img
           src="https://camo.githubusercontent.com/1844e7e948600538af6a21c77ab62427589ddfd3c003691d91fad094631fd0f7/68747470733a2f2f692e696d6775722e636f6d2f5237536a4269372e676966"
           alt=""
@@ -100,7 +150,7 @@ const Portfolio = ({ inEsp, darkMode }) => {
         </div>
       </div>
 
-      <div className="flex justify-center flex-col lg:flex-row items-center mb-5 w-full 2xl:w-4/5 m-auto p-2 mt-8">
+      <div className="flex justify-center flex-col lg:flex-row items-center mb-5 w-full 2xl:w-11/12 m-auto p-2 mt-8">
         <img src="ram.png" alt="" className="w:full lg:w-1/2 " />
         <div className="w-full lg:w-1/2 p-8 text-lg">
           <p className={darkMode ? "text-white" : ""}>
